36466f5167b28ffb26799bacc97af2f6a0e76437
andy seroff <aseroff@umn.edu>
2019-07-15 18:15:14 -0500

    Updates

---
+++ precache-manifest.068404709ebc902f51203fb5324d3465.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51357b087f880889028ee361d5a8fc2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef6a3b9d1d980e46be4",
+    "url": "/static/css/main.748c5540.chunk.css"
+  },
+  {
+    "revision": "82425bced575c76f8753",
+    "url": "/static/js/2.42340fb5.chunk.js"
+  },
+  {
+    "revision": "2ef6a3b9d1d980e46be4",
+    "url": "/static/js/main.60c46712.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "89576a0ab1a83b33d804da9697b5a699",
+    "url": "/static/media/bach.89576a0a.png"
+  },
+  {
+    "revision": "ca78b52ada7dcad8ad6f6dcda65bbf60",
+    "url": "/static/media/cfdb.ca78b52a.png"
+  },
+  {
+    "revision": "4fa3bf5371457f1e0416a67c48e3df51",
+    "url": "/static/media/myguy.4fa3bf53.png"
+  },
+  {
+    "revision": "8fd6aecded7de4b48448eef6e9dec21e",
+    "url": "/static/media/nextguide.8fd6aecd.png"
+  },
+  {
+    "revision": "804831be4d4341a4cc2643d9d9bebcbe",
+    "url": "/static/media/ocho.804831be.png"
+  },
+  {
+    "revision": "a5a107806dc146ddc0f8bf62c3756562",
+    "url": "/static/media/otl.a5a10780.png"
+  },
+  {
+    "revision": "9c0de54ff1edae3eeb9531b5f59c56eb",
+    "url": "/static/media/pic.9c0de54f.png"
+  },
+  {
+    "revision": "08ad6acd346d7559a1d519510a8ec916",
+    "url": "/static/media/quips.08ad6acd.png"
+  },
+  {
+    "revision": "b28da3f53b22f79b754cb897cf19274a",
+    "url": "/static/media/sideshow.b28da3f5.png"
+  },
+  {
+    "revision": "71f3bfb4ba6d89b76dab618d8a1529f6",
+    "url": "/static/media/tndrbox.71f3bfb4.png"
+  },
+  {
+    "revision": "b043e872711d4beb0fe685f12d439eff",
+    "url": "/static/media/wedding.b043e872.png"
+  }
+]);
